ab2a366240aefc5e92876d1ab9a8faaf4ac699ef
nkorim321-creator <nkorim321@gmail.com>
2026-08-07 01:18:04 +0600

    Update BP.user.js

--- BP.user.js
+++ BP.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         MTurk Human-Like Rater 28.8
+// @name         MTurk Human-Like Rater 28.5
 // @namespace    http://tampermonkey.net/
 // @version      28.5
 // @description  Photo-specific comments, no default notes, slow typing, full anti-detection
